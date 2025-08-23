514d23985a751418e06c68e64858ef6b57152524
Jaczina Norbert <apple@MacBookPro.home>
2025-08-23 11:14:49 +0200

    Creating the authentication for the application & signing the user in

--- Spotify/Managers/AuthManager.swift
+++ Spotify/Managers/AuthManager.swift
@@ -10,7 +10,20 @@ import Foundation
 final class AuthManager {
     static let shared = AuthManager()
     
+    struct Constants {
+        static let clientID = "4787d90085334e1db900d02bf7a3f8d9"
+        static let clientSecret = "e78b37f3f2f84eecad479ba0584e24d4"
+    }
+    
     private init() {}
+    
+    public var signInURL: URL? {
+        let scopes = "user-read-private"
+        let redirectURI = "https://open.spotify.com"
+        let base = "https://accounts.spotify.com/authorize"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        return URL(string: string)
+    }
     
     var isSignedIn: Bool {
         return false
@@ -30,5 +43,17 @@ final class AuthManager {
     
     private var shouldRefreshToken: Bool {
         return false
+    }
+    
+    public func exchangeCodeForToken(code: String, completion: @escaping (Bool) -> Void) {
+        
+    }
+    
+    public func refreshAccessToken() {
+        
+    }
+    
+    private func cacheToken() {
+        
     }
 }
